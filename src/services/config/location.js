f2174495147a45ffb69059cc77fb5626af6ab882
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2019-09-11 20:30:49 +0700

    fix version 1.1.0

--- src/services/config/location.js
+++ src/services/config/location.js
@@ -1,5 +1,7 @@
-export const LATITUDE = '-8.033916'
-export const LONGITUDE = '112.649420'
+// poltekom
+// export const LATITUDE = '-8.033916'
+// export const LONGITUDE = '112.649420'
 
-// lat: -7.761506, //koordinat pemkab
-// long: 113.4156582, //koordinat pemkab
+// pemkab lamongan
+export const LATITUDE = '-7.761506'
+export const LONGITUDE = '113.4156582'
--- src/services/config/path.js
+++ src/services/config/path.js
@@ -1,6 +1,6 @@
 // export const RootPath = 'http://10.0.3.2/absensi/public/api'
-export const RootPath = 'https://apel.limakode.com/api'
-export const WebPath = 'https://apel.limakode.com/'
+// export const RootPath = 'https://apel.limakode.com/api'
+// export const WebPath = 'https://apel.limakode.com/'
 
-// export const RootPath = 'https://103.124.88.180/api'
-// export const WebPath = 'https://103.124.88.180/'
+export const RootPath = 'http://103.124.88.180/api'
+export const WebPath = 'http://103.124.88.180/'
