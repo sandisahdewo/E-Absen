402db0cd202f883d247b99c95448ad591618fc38
Sandi Sahdewo <sandi.sahdewo@gmail.com>
2019-09-11 14:07:32 +0700

    ganti nama aplikasi

---
+++ src/services/config/location.js
@@ -0,0 +1,5 @@
+export const LATITUDE = '-8.033916'
+export const LONGITUDE = '112.649420'
+
+// lat: -7.761506, //koordinat pemkab
+// long: 113.4156582, //koordinat pemkab
--- src/services/config/path.js
+++ src/services/config/path.js
@@ -1,5 +1,5 @@
-export const RootPath = 'http://10.0.3.2/absensi/public/api'
-// export const RootPath = 'https://apel.limakode.com/api'
+// export const RootPath = 'http://10.0.3.2/absensi/public/api'
+export const RootPath = 'https://apel.limakode.com/api'
 export const WebPath = 'https://apel.limakode.com/'
 
 // export const RootPath = 'https://103.124.88.180/api'
